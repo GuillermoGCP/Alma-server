e7930340fcefef8085cf0898383dd24eeb8d0a11
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2025-01-24 19:22:17 +0100

    FormsData translate

--- src/controllers/forms/getAllForms.js
+++ src/controllers/forms/getAllForms.js
@@ -1,20 +1,27 @@
 import { allSheetData } from '../../googleapis/methods/index.js'
+import { allFormsObjectCreator } from '../../helpers/formObject.js'
 import { groupDataById } from '../../utils/index.js'
 
 const getAllForms = async (req, res, next) => {
-    try {
-        const spreadsheetId = process.env.SPREADSHEET_ID
+  try {
+    const spreadsheetId = process.env.SPREADSHEET_ID
 
-        // Traigo todos los datos de la hoja de formularios:
-        const data = await allSheetData(spreadsheetId, 'Formularios')
-        const { rows } = data
+    // Traigo todos los datos de la hoja de formularios:
+    const data = await allSheetData(spreadsheetId, 'Formularios')
+    const { rows } = data
 
-        // Agrupo los formularios por id:
-        const groupedData = groupDataById(rows)
+    // Agrupo los formularios por id:
+    const groupedData = groupDataById(rows)
 
-        res.send({ message: 'formularios obtenidos', forms: groupedData })
-    } catch (error) {
-        next(error)
-    }
+    // Traduzco los nombres y los campos de los formularios:
+    const translatedData = await allFormsObjectCreator(groupedData)
+
+    res.send({
+      message: 'formularios obtenidos',
+      forms: translatedData,
+    })
+  } catch (error) {
+    next(error)
+  }
 }
 export default getAllForms
--- src/controllers/forms/getFormById.js
+++ src/controllers/forms/getFormById.js
@@ -2,51 +2,55 @@ import { getRowsData } from '../../googleapis/methods/index.js'
 import groupDataById from '../../utils/groupDataById.js'
 import { unnormalizeFieldName } from '../../utils/index.js'
 import FormModel from '../../Database/models/FormModel.js'
+import { formObjectCreator } from '../../helpers/formObject.js'
 
 const getFormById = async (req, res, next) => {
-    try {
-        const formId = req.params.formId
-        const publish = req.params.publish
-        const jsonNumber = req.params.jsonNumber
-        const spreadsheetId = process.env.SPREADSHEET_ID
-        let formToSave
-        const fields = {
-            field: 'id',
-            value: formId,
-            sheetName: 'Formularios',
-        }
-
-        const values = await getRowsData(spreadsheetId, 'Formularios', fields)
-        const { rowsData } = values
-
-        const form = groupDataById(rowsData)
-
-        //Formateo los datos para que coincidan con la estructura esperada en el front:
-        const dataToSend = {
-            formName: unnormalizeFieldName(Object.entries(form)[0][1].formName),
-            formId: formId,
-            publishNumber: jsonNumber,
-            fields: Object.entries(form)[0][1].fields.map((field) => {
-                return {
-                    label: unnormalizeFieldName(field.label),
-                    type: field.type,
-                }
-            }),
-        }
-
-        //Si se publica, se guarda en Mongo:
-        if (publish === 'publish') {
-            formToSave = new FormModel(dataToSend)
-            await formToSave.save()
-        }
-
-        res.send({
-            message: `formulario con id: ${formId} obtenido`,
-            form: dataToSend,
-        })
-    } catch (error) {
-        console.log(error)
-        next(error)
+  try {
+    const formId = req.params.formId
+    const publish = req.params.publish
+    const jsonNumber = req.params.jsonNumber
+    const spreadsheetId = process.env.SPREADSHEET_ID
+    let formToSave
+    const fields = {
+      field: 'id',
+      value: formId,
+      sheetName: 'Formularios',
     }
+
+    const values = await getRowsData(spreadsheetId, 'Formularios', fields)
+    const { rowsData } = values
+
+    const form = groupDataById(rowsData)
+
+    //Formateo los datos para que coincidan con la estructura esperada en el front:
+    const dataToSend = {
+      formName: unnormalizeFieldName(Object.entries(form)[0][1].formName),
+      formId: formId,
+      publishNumber: jsonNumber,
+      fields: Object.entries(form)[0][1].fields.map((field) => {
+        return {
+          label: unnormalizeFieldName(field.label),
+          type: field.type,
+        }
+      }),
+    }
+
+    //Traduzco los campos del formulario:
+    const formTranslated = await formObjectCreator(dataToSend)
+
+    //Si se publica, se guarda en Mongo:
+    if (publish === 'publish') {
+      formToSave = new FormModel(formTranslated)
+      await formToSave.save()
+    }
+
+    res.send({
+      message: `formulario con id: ${formId} obtenido`,
+      form: formTranslated,
+    })
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
 }
 export default getFormById
